a90d09dd6025f1d1d47114c4aecf91c2e6ed5b49
Ling He <szlghl1@163.com>
2017-05-04 02:38:05 -0400

    Deleted h2 dependency. It once could not run without it, because spring boot cannot determine the db we were using since both jpa and mongodb were imported. Update origin of annotation Id. Then we do not need jpa dependency.

--- order/src/main/java/demo/domain/Order.java
+++ order/src/main/java/demo/domain/Order.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
